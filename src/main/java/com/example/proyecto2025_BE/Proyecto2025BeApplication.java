7cea4db4be9fdb13f49eebfd339000781ff4a2ab
JulianGibelli <juligibelli@gmail.com>
2025-03-08 19:12:43 -0300

    initial commit BE repo

---
+++ src/main/java/com/example/proyecto2025_BE/Proyecto2025BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto2025_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2025BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2025BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto2025_BE/Proyecto2025BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto2025_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2025BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
