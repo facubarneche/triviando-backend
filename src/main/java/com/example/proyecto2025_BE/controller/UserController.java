9d4e78daf4acb22b7a801270be3b72f7c34df4ca
facu <faq.n.s@gmail.com>
2025-03-30 14:48:00 -0300

    [FEATURE] CRUD - users

---
+++ src/main/java/com/example/proyecto2025_BE/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.example.proyecto2025_BE.controller;
+
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.proyecto2025_BE.model.User;
+import com.example.proyecto2025_BE.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@CrossOrigin(origins = "*")	// TODO: Cambiar luego por los dominios que permitiremos que consuman esta api
+@RequiredArgsConstructor
+public class UserController {
+	
+	private final UserService userService;
+	
+	@PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+    	this.userService.create(user);
+        
+        return ResponseEntity.ok(user);
+    }
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<User> retrieve(@PathVariable Long id) {
+		User user = this.userService.retrieve(id);
+		
+		return ResponseEntity.ok(user);
+	}
+	
+	@PutMapping
+    public ResponseEntity<User> update(@RequestBody User user) {
+    	this.userService.update(user);
+
+		return ResponseEntity.ok(user);
+    }
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> delete(@PathVariable Long id) {
+		this.userService.delete(id);
+
+		return ResponseEntity.ok(Strings.EMPTY);
+	}
+	
+	@PostMapping
+    public ResponseEntity<User> login(@RequestBody User user) {
+		User logged = this.userService.findByEmailAndPassword(user);
+        
+        return ResponseEntity.ok(logged);
+    }
+}
---
+++ src/main/java/com/example/proyecto2025_BE/service/UserService.java
@@ -0,0 +1,52 @@
+package com.example.proyecto2025_BE.service;
+
+
+import org.springframework.stereotype.Service;
+
+import com.example.proyecto2025_BE.constants.Exceptions;
+import com.example.proyecto2025_BE.dao.UserDao;
+import com.example.proyecto2025_BE.exceptions.ConflictException;
+import com.example.proyecto2025_BE.exceptions.NotFoundException;
+import com.example.proyecto2025_BE.model.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+	
+	private final UserDao userDao;
+	
+	public void create(User user) {
+		userDao.findByEmail(user.getEmail())
+			.orElseThrow(() -> ConflictException.build("El usuario ya existe en el sistema."));
+		
+		userDao.save(user);
+    }
+	
+	public User retrieve(Long id) {
+		return userDao.findById(id)
+			.orElseThrow(() -> NotFoundException.build(Exceptions.NOT_FOUND));
+	}
+	
+	public void update(User user) {
+		validateById(user.getId());
+		userDao.save(user);
+    }
+	
+	private void validateById(Long id) {
+		userDao.findById(id)
+			.orElseThrow(() -> NotFoundException.build(Exceptions.NOT_FOUND));
+	}
+	
+	public void delete(Long id) {
+		User user = retrieve(id);
+		userDao.delete(user);
+    }
+	
+	public User findByEmailAndPassword(User user) {
+		return this.userDao.findByEmailAndPassword(user.getEmail(), user.getPassword())
+				.orElseThrow(() -> NotFoundException.build(Exceptions.NOT_FOUND));
+		
+	}
+}
