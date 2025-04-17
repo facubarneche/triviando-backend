eb0802812ef9b7471070c7325db9a5c84ee243a7
facu <faq.n.s@gmail.com>
2025-04-16 21:53:15 -0300

    [FEATURE] response in json format

--- src/main/java/com/example/proyecto2025_BE/service/ApiClient.java
+++ src/main/java/com/example/proyecto2025_BE/service/ApiClient.java
@@ -1,26 +1,20 @@
 package com.example.proyecto2025_BE.service;
 
-import java.net.Authenticator;
-import java.net.PasswordAuthentication;
-import java.net.http.HttpClient;
 import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
-import dev.langchain4j.service.SystemMessage;
 import org.springframework.stereotype.Service;
 
-import dev.langchain4j.http.client.jdk.JdkHttpClient;
-import dev.langchain4j.http.client.jdk.JdkHttpClientBuilder;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.chat.request.ResponseFormat;
 import dev.langchain4j.model.ollama.OllamaStreamingChatModel;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.TokenStream;
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Flux;
 
 @Slf4j
 @Service
@@ -72,39 +66,8 @@ public class ApiClient implements ChatLanguageModel {
 		return future;
 	}
 
-//    public Flux<Object> post(String topic) {
-//    	 return Flux.create(sink -> {
-//    		 String prompt = TEMPLATE_PROMPT + topic;
-//	    	 TokenStream tokenStream = chatWithModel(prompt);
-//	    	 StringBuilder fullResponse = new StringBuilder("");
-//
-//             tokenStream.onPartialResponse(partialResponse -> {
-//                         sink.next(partialResponse);
-//                         fullResponse.append(partialResponse);
-//                     })
-//                     .onCompleteResponse(response -> {
-//                         sink.complete();
-//                         log.info("Respuesta completa (TokenStream): {}", fullResponse.toString());
-//                     })
-//                     .onError(sink::error)
-//                     .start();
-//         });
-//    }
-
 	//TODO: pasar credenciales a el application.yml
     private StreamingChatLanguageModel connectModel(String modelUrl, String modelName) {
-//		HttpClient.Builder httpClientBuilder = HttpClient.newBuilder()
-//				.authenticator(new Authenticator() {
-//					@Override
-//					protected PasswordAuthentication getPasswordAuthentication() {
-//						return new PasswordAuthentication("gemma3", ("LosPibitosDeLaUnsamMandan.".toCharArray()));
-//					}
-//				});
-//    	JdkHttpClientBuilder jdkHttpClientBuilder = JdkHttpClient.builder()
-//    	        .httpClientBuilder(httpClientBuilder)
-//				.connectTimeout(Duration.ofMinutes(2))
-//				.readTimeout(Duration.ofMinutes(2));
-
 		return OllamaStreamingChatModel.builder()
     	        .baseUrl(modelUrl)
     	        .modelName(modelName)
@@ -112,6 +75,7 @@ public class ApiClient implements ChatLanguageModel {
 				.logRequests(Boolean.TRUE)
 				.logResponses(Boolean.TRUE)
     	        .timeout(Duration.ofMinutes(1))
+    	        .responseFormat(ResponseFormat.JSON)
     	        .build();
     }
 }
