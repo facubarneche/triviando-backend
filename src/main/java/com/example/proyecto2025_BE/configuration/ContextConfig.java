08542951350073dfb0d1320d1c2c7bfb22f75059
facu <faq.n.s@gmail.com>
2025-04-19 22:07:00 -0300

    [FEATURE] REfactor llm api client

--- src/main/java/com/example/proyecto2025_BE/configuration/ContextConfig.java
+++ src/main/java/com/example/proyecto2025_BE/configuration/ContextConfig.java
@@ -1,7 +1,5 @@
 package com.example.proyecto2025_BE.configuration;
 
-import java.net.http.HttpClient;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +7,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class ContextConfig {
-
-	@Bean
-	public HttpClient httpClient() {
-		return HttpClient.newHttpClient();
-	}
 	
 	@Bean
 	public ObjectMapper objectMapper() {
