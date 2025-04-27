e6fcba0436736973ad83f79ed531e30cbefd523e
Fedes23 <150834895+DserafiniGiraudo@users.noreply.github.com>
2025-04-27 18:36:34 -0300

    [REFACTOR] cantidad preguntas por topico

--- src/main/java/com/example/proyecto2025_BE/dao/TopicoDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.proyecto2025_BE.dao;
-
-import com.example.proyecto2025_BE.model.Topico;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TopicoDao extends MongoRepository<Topico, String> {}
