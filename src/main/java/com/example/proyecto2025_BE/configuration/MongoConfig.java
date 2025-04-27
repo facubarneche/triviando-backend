cb698f0eb9fae3d1ea62f0910f85e6c92181a103
facu <faq.n.s@gmail.com>
2025-04-26 23:23:34 -0300

    updated profiles for MongoConfig script execution

--- src/main/java/com/example/proyecto2025_BE/configuration/MongoConfig.java
+++ src/main/java/com/example/proyecto2025_BE/configuration/MongoConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
-@Profile({"dev", "test"})
+@Profile("dev")
 public class MongoConfig {
 
     @Bean
