9a564007232728b95ac8152e39fbca0c3fad120f
facu <faq.n.s@gmail.com>
2025-05-13 18:57:19 -0300

    fix conflicts

--- src/main/java/com/example/proyecto2025_BE/constants/DatePattern.java
+++ src/main/java/com/example/proyecto2025_BE/constants/DatePattern.java
@@ -2,4 +2,5 @@ package com.example.proyecto2025_BE.constants;
 
 public class DatePattern {
 	public static final String DATE = "yyyy-MM-dd";
+	public static final String DATE_TIME = "yyyy-MM-dd HH:mm:ss";
 }
