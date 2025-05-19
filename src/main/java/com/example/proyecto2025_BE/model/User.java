66343a82ca25a977d314488a2f77e705d216cdb8
Fedes23 <150834895+DserafiniGiraudo@users.noreply.github.com>
2025-05-18 21:08:52 -0300

    [REFACTOR] Se elimina clase basura y se agrega un getter con valor 0 por defecto para rachaActual en user

--- src/main/java/com/example/proyecto2025_BE/model/User.java
+++ src/main/java/com/example/proyecto2025_BE/model/User.java
@@ -98,4 +98,12 @@ public class User {
 		this.rachaActual = strategy.calcularRacha(rachaActual);
 		this.ultimaActividad = today;
 	}
+
+	public Integer getRachaActual() {
+		return rachaActual == null ? 0 : rachaActual;
+	}
+
+	public LocalDate getUltimaActividad() {
+		return ultimaActividad == null ? createdAt.toLocalDate() : ultimaActividad;
+	}
 }
