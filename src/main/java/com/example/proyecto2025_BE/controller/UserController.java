85535b048ed2b32c9085f77b6bd428ddfc2e32e4
Fedes23 <150834895+DserafiniGiraudo@users.noreply.github.com>
2025-06-06 15:40:40 -0300

    [FIX] ajuste ranking semanal

--- src/main/java/com/example/proyecto2025_BE/controller/UserController.java
+++ src/main/java/com/example/proyecto2025_BE/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.proyecto2025_BE.controller;
 
 import java.util.Map;
 
+import com.example.proyecto2025_BE.model.dto.Ranking;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -175,14 +176,13 @@ public class UserController {
 	}
 
 	@GetMapping("/ranking-semanal")
-	@JsonView(Views.Ranking.class)
 	@Operation(summary = "Ranking de usuarios", description = "Devuelve el ranking semanal de usuarios")
 	@ApiResponses( value = {
 			@ApiResponse(responseCode = "200", description = "Ranking semanal obtenido",
 					content = @Content(mediaType = "application/json", schema = @Schema(implementation = PageUserRankingResponse.class))),
 			@ApiResponse(responseCode = "404", description = Exceptions.NOT_FOUND,
 					content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse4XX.class)))})
-	public ResponseEntity<Page<User>> getWeeklyRanking(
+	public ResponseEntity<Page<Ranking>> getWeeklyRanking(
 			@RequestParam(defaultValue = "0") int page,
 			@RequestParam(defaultValue = "10") int size) {
 		return ResponseEntity.ok(userService.getWeeklyRanking(page, size));
