b18469610deb176b57b81b6a04a3b93cbf546628
JuanCaceffo <caceffojuan@gmail.com>
2025-06-08 21:23:03 -0300

    [FEAT] -> added methods to generate emoji

--- src/main/java/com/example/proyecto2025_BE/model/dto/llm/Question.java
+++ src/main/java/com/example/proyecto2025_BE/model/dto/llm/Question.java
@@ -6,7 +6,6 @@ import com.example.proyecto2025_BE.model.Difficulty;
 
 public record Question(
 		String text,
-		String emoji,
 		List<QuestionOption> options, 
 		QuestionOption correctOption,
 		Difficulty difficulty,
