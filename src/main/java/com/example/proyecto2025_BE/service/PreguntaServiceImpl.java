fd60b62e3b60089c8b7d7f317574b4fffef1b174
JuanCaceffo <caceffojuan@gmail.com>
2025-06-13 19:40:12 -0300

    [IMPRV] -> added builder to topic DTO and removed unused imports

--- src/main/java/com/example/proyecto2025_BE/service/PreguntaServiceImpl.java
+++ src/main/java/com/example/proyecto2025_BE/service/PreguntaServiceImpl.java
@@ -1,13 +1,9 @@
 package com.example.proyecto2025_BE.service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
 import com.example.proyecto2025_BE.model.dto.Topics;
 import org.springframework.stereotype.Service;
-
 import com.example.proyecto2025_BE.dao.PreguntaDao;
 import com.example.proyecto2025_BE.exceptions.NotFoundException;
 import com.example.proyecto2025_BE.model.Answer;
@@ -16,10 +12,6 @@ import com.example.proyecto2025_BE.model.dto.PreguntaRequest;
 import com.example.proyecto2025_BE.service.pregunta.factory.PreguntaLoaderFactory;
 import com.example.proyecto2025_BE.service.pregunta.strategy.PreguntaLoaderStrategy;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -58,11 +50,12 @@ public class PreguntaServiceImpl implements PreguntaService {
     public List<Topics> contarPreguntasPorTopico() {
         return preguntaDao.contarPreguntasPorTopico().stream()
                 .map( topic ->
-                    new Topics(
-                            (String) topic.get("_id"),
-                            (Number) topic.get("cantidadPreguntas"),
-                            (String) topic.get("emoji")))
-                .toList();
+                    Topics.builder()
+                            .topic((String) topic.get("_id"))
+                            .size((Number) topic.get("cantidadPreguntas"))
+                            .emoji((String) topic.get("emoji"))
+                            .build()
+                ).toList();
     }
 
     @Override
