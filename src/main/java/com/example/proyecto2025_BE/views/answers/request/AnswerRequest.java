f987c0d35c96f3a793c26d7fd66514e68c34fc9a
Fedes23 <150834895+DserafiniGiraudo@users.noreply.github.com>
2025-07-12 01:36:55 -0300

    [refactor] refactor documentacion

--- src/main/java/com/example/proyecto2025_BE/views/answers/request/AnswerRequest.java
+++ src/main/java/com/example/proyecto2025_BE/views/answers/request/AnswerRequest.java
@@ -2,6 +2,7 @@ package com.example.proyecto2025_BE.views.answers.request;
 
 import com.example.proyecto2025_BE.model.LetterOption;
 
+import com.example.proyecto2025_BE.model.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +14,9 @@ public class AnswerRequest {
 	 	
 	@Schema(description = "Id de pregunta respondida", example = "bb5c6bf4-4dec-4ab7-b3fc-ac34940ccf6d")
 	private String questionId;
-	@Schema(description = "Id del usuario que responde", example = "2373245246753")
-    private Long userId;
+	@Schema(description = "Usuario con su información",
+			example = "{\"id\": 123}")
+	private User user;
 	@Schema(description = "Letra de la opcion seleccionada", example = "A")
     private LetterOption optionSelected;
 	@Schema(description = "Tiempo en milisegundos empleados en responder", example = "25000")
